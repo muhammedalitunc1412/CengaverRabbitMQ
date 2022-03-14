80f302fb6a342c3810a8dbc3a66c7860fafc4c1a
muhammed1412 <muhammed@retrace.ai>
2022-03-14 21:46:49 +0300

    Add project files.

---
+++ CengaverRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CengaverRabbitMQ.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue", false, false, false);
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                var message = $"{x}. Message";
+
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+                Console.WriteLine($"{x}. Message sent successfully");
+
+            });
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UdemyRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace UdemyRabbitMQ.Subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            // channel.QueueDeclare("hello-queue", false, false, false);
+
+            channel.BasicQos(0,1,false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume("hello-queue", false, consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+             {
+                 var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                 
+                 Console.WriteLine($"Message is: {message}");
+                 Thread.Sleep(1500);
+
+                 channel.BasicAck(e.DeliveryTag, false);
+             };
+
+            Console.ReadLine();
+        }
+    }
+}
