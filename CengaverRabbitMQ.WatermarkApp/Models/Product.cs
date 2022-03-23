00990b411645f54be8a8a3681a4ede616a28ff87
muhammed1412 <muhammed@retrace.ai>
2022-03-23 16:15:14 +0300

    Add db

---
+++ CengaverRabbitMQ.WatermarkApp/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CengaverRabbitMQ.WatermarkApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id{ get; set; }
+        public string Name{ get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
