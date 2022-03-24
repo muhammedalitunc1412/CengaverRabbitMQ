f98e8cfffa4869921e17c1e3dd319127b5537c5e
muhammed1412 <muhammed@retrace.ai>
2022-03-24 13:21:06 +0300

    Add ClientService

--- CengaverRabbitMQ.WatermarkApp/Startup.cs
+++ CengaverRabbitMQ.WatermarkApp/Startup.cs
@@ -1,4 +1,5 @@
 using CengaverRabbitMQ.WatermarkApp.Models;
+using CengaverRabbitMQ.WatermarkApp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +27,8 @@ namespace CengaverRabbitMQ.WatermarkApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(Configuration.GetConnectionString("RabbitMQ")) });
+            services.AddSingleton<RabbitMQClientService>();
             services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseInMemoryDatabase(databaseName: "productDb");
